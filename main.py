e94ec7623590b8840e7ad89069ff97c7c804ccc6
Attreya Bhatt <attreya01@gmail.com>
2024-11-24 10:30:47 +0530

    added score

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ country_domain_file.close()
 
 number_of_countries_tld = len(data)
 
+score = 0
 while True:
     random_number = random.randrange(0,number_of_countries_tld)
     country_tuple, domain_tuple = data[random_number].items()
-    country_name = country_tuple[1].lower()
+    country_name = country_tuple[1]
     domain_name = domain_tuple[1]
 
     print("Domain name is: " + domain_name)
@@ -18,9 +19,15 @@ while True:
     country_name_user_input = country_name_user_input.lower()
 
     if country_name is not None and domain_name is not None:
-        if country_name_user_input == country_name:
+        if country_name_user_input == country_name.lower():
             print("Correct")
+            score = score + 1
         else:
             print("Wrong. The correct answer is: " + country_name)
-
+            score = score - 1
+        if score > 0 :
+            print("Current Score: " + str(score))
+        else:
+            print("Current Score: 0")
+        
 
